2a4dbb41ad4dbcd2cea6facbe82ff0cccf0f46fe
wm265404 <wm265404@alibaba-inc.com>
2020-08-14 20:35:51 +0800

    add more features

--- src/components/time-picker.jsx
+++ src/components/time-picker.jsx
@@ -1,11 +1,16 @@
-import React, {useState, useCallback, useEffect, useMemo, useRef} from 'react';
+import React, {useState, useCallback, useMemo, useRef, useEffect} from 'react';
 import propTypes from 'prop-types';
 import styles from 'static/index.less';
 import hint from 'static/hint.svg';
 let width, height, fontSize;
-export default function TimePicker({size, zIndex, setTimeRange, title}) {
-	const [selectedCell, setSelectedCell] = useState([null, null]);
+export default function TimePicker({size, zIndex, setTime, title, attachElement}) {
+	const [selectedRange, setSelectedRange] = useState([null, null]);
+	const [visible, setVisible] = useState(false);
 	const ref = useRef(null);
+	const positionRef = useRef(null);
+	const container = useMemo(() => {
+		return React.cloneElement(attachElement, {onClick: () => setVisible(true), ref: positionRef})
+	}, [attachElement])
 	const width = useMemo(() => {
 		switch (size) {
 			case 'small':
@@ -33,18 +38,18 @@ export default function TimePicker({size, zIndex, setTimeRange, title}) {
 	const height = useMemo(() => {
 		switch (size) {
 			case 'small':
-				return '500px'
+				return '400px'
 			case 'medium':
-				return '600px';
+				return '500px';
 			case 'big':
 				return '800px';
 			default:
-				return '500px';
+				return '400px';
 		}
 	}, [size]);
 
 	const index = useMemo(() => {
-		const logo = [<img src = {hint} alt = 'H/M' style = {{width: '100%', height: '100%'}}/>];
+		const logo = [<div key="hint" className = {styles.index}><img src = {hint} alt = 'H/M' style = {{width: '100%', height: '100%'}}/></div>];
 		const column = [];
 		for (let i = 0; i < 60; i++) {
 			column.push(<div key = {`column-${i}`} className = {styles.index} style = {{gridArea: `1 / ${i + 2} / 2 / ${i + 3}`}}> {i} </div>)
@@ -58,63 +63,71 @@ export default function TimePicker({size, zIndex, setTimeRange, title}) {
 
 	const handleCellClick = useCallback(e => {
 		e.preventDefault();
-		const indexOffSet = index.length;
-		let nextSelectedCell = selectedCell.map(v => v);
-		if (selectedCell[0] !== null && selectedCell[1] !== null) {
-			nextSelectedCell = [null, null];
-			nextSelectedCell[0] = e.target.dataset.id;
-			let recover = [];
-			if (Number.parseInt(selectedCell[0]) < Number.parseInt(selectedCell[1]))
-				recover = [...ref.current.children].slice(indexOffSet + Number.parseInt(selectedCell[0]), indexOffSet + Number.parseInt(selectedCell[1]) + 1);
-			else if (Number.parseInt(selectedCell[0]) > Number.parseInt(selectedCell[1]))
-				recover = [...ref.current.children].slice(indexOffSet, indexOffSet + Number.parseInt(selectedCell[1]) + 1)
-					.concat([...ref.current.children].slice(indexOffSet + Number.parseInt(selectedCell[0]), [...ref.current.children].length))
-			else recover = [[...ref.current.children][indexOffSet + Number.parseInt(selectedCell[0])]];
-			recover.forEach(cell => cell.className = styles['cell']);
-			e.target.className = styles['cell-selected'];
-		} else if (selectedCell[0] !== null) {
-			nextSelectedCell[1] = e.target.dataset.id;
-			let cover, first, last;
-			if (Number.parseInt(nextSelectedCell[1]) > Number.parseInt(nextSelectedCell[0])) {
-				cover = [...ref.current.children].slice(indexOffSet + Number.parseInt(nextSelectedCell[0]), indexOffSet + Number.parseInt(nextSelectedCell[1]) + 1);
-				first = cover.shift();
-				last = cover.pop();
-			} else if (Number.parseInt(nextSelectedCell[1]) < Number.parseInt(nextSelectedCell[0])) {
-				cover = [...ref.current.children].slice(indexOffSet, indexOffSet + Number.parseInt(nextSelectedCell[1]))
-					.concat([...ref.current.children].slice(indexOffSet + Number.parseInt(nextSelectedCell[0]) + 1, [...ref.current.children].length))
-				first = [...ref.current.children][indexOffSet + Number.parseInt(nextSelectedCell[1])];
-				last = [...ref.current.children][indexOffSet + Number.parseInt(selectedCell[0])];
+		const target = e.target;
+			const indexOffSet = index.length;
+			let nextSelectedCell = selectedRange.map(v => v);
+			if (selectedRange[0] !== null && selectedRange[1] !== null) {
+				nextSelectedCell = [null, null];
+				nextSelectedCell[0] = target.dataset.id;
+				let recover = [];
+				if (Number.parseInt(selectedRange[0]) < Number.parseInt(selectedRange[1]))
+					recover = [...ref.current.children].slice(indexOffSet + Number.parseInt(selectedRange[0]), indexOffSet + Number.parseInt(selectedRange[1]) + 1);
+				else if (Number.parseInt(selectedRange[0]) > Number.parseInt(selectedRange[1]))
+					recover = [...ref.current.children].slice(indexOffSet, indexOffSet + Number.parseInt(selectedRange[1]) + 1)
+						.concat([...ref.current.children].slice(indexOffSet + Number.parseInt(selectedRange[0]), [...ref.current.children].length))
+				else recover = [[...ref.current.children][indexOffSet + Number.parseInt(selectedRange[0])]];
+				recover.forEach(cell => cell.className = styles['cell']);
+				target.className = styles['cell-selected'];
+			} else if (selectedRange[0] !== null) {
+				nextSelectedCell[1] = target.dataset.id;
+				let cover, first, last;
+				if (Number.parseInt(nextSelectedCell[1]) > Number.parseInt(nextSelectedCell[0])) {
+					cover = [...ref.current.children].slice(indexOffSet + Number.parseInt(nextSelectedCell[0]), indexOffSet + Number.parseInt(nextSelectedCell[1]) + 1);
+					first = cover.shift();
+					last = cover.pop();
+				} else if (Number.parseInt(nextSelectedCell[1]) < Number.parseInt(nextSelectedCell[0])) {
+					cover = [...ref.current.children].slice(indexOffSet, indexOffSet + Number.parseInt(nextSelectedCell[1]))
+						.concat([...ref.current.children].slice(indexOffSet + Number.parseInt(nextSelectedCell[0]) + 1, [...ref.current.children].length))
+					first = [...ref.current.children][indexOffSet + Number.parseInt(nextSelectedCell[1])];
+					last = [...ref.current.children][indexOffSet + Number.parseInt(selectedRange[0])];
+				} else {
+					cover = [];
+					first = last = {};
+				}
+				first.className = last.className = styles['cell-selected'];
+				cover.forEach(cell => cell.className = styles['cell-included']);
+				setVisible(false);
 			} else {
-				cover = [];
-				first = last = {};
+				nextSelectedCell[0] = target.dataset.id;
+				target.className = styles['cell-selected'];
 			}
-			first.className = last.className = styles['cell-selected'];
-			cover.forEach(cell => cell.className = styles['cell-included']);
-		} else {
-			nextSelectedCell[0] = e.target.dataset.id;
-			e.target.className = styles['cell-selected'];
-		}
-		setSelectedCell(nextSelectedCell);
-	}, [selectedCell, index])
+			setSelectedRange(nextSelectedCell);
+			const begin_res = Number.parseInt(nextSelectedCell[0]);
+			const end_res = Number.parseInt(nextSelectedCell[1]);
+			const begin = begin_res >= 0 ? `${Math.floor(begin_res / 60)}:${begin_res % 60 < 10 ? '0'.concat(begin_res % 60) : begin_res % 60}` : '';
+			const end = end_res >= 0 ? `${Math.floor(end_res / 60)}:${end_res % 60 < 10 ? '0'.concat(end_res % 60) : end_res % 60}` : '';
+			setTime(`${begin}-${end}`)
+	}, [selectedRange, index])
 
 	const handleHoverHighLight = useCallback((e, flag) => {
+		e.preventDefault();
+		const indexOffset = index.length;
 		const imgOffset = 1;
 		const columnOffset = 60;
 		const highLightColumnIndex = ref.current.children[imgOffset + e.target.dataset.id % 60];
 		const highLightRowIndex = ref.current.children[imgOffset + columnOffset + Math.floor(e.target.dataset.id / 60)];
 		highLightColumnIndex.className = highLightRowIndex.className = flag ? styles['index-hover'] : styles['index'];
-		const indexOffset = index.length;
-		if (selectedCell[0] !== null && selectedCell[1] === null) {
+		if (selectedRange[0] !== null && selectedRange[1] === null) {
 			let cover = [];
-			if (Number.parseInt(e.target.dataset.id) > Number.parseInt(selectedCell[0]))
-				cover = [...ref.current.children].slice(indexOffset + Number.parseInt(selectedCell[0]) + 1, indexOffset + Number.parseInt(e.target.dataset.id))
-			else if (Number.parseInt(e.target.dataset.id) < Number.parseInt(selectedCell[0]))
+			if (Number.parseInt(e.target.dataset.id) > Number.parseInt(selectedRange[0]))
+				cover = [...ref.current.children].slice(indexOffset + Number.parseInt(selectedRange[0]) + 1, indexOffset + Number.parseInt(e.target.dataset.id))
+			else if (Number.parseInt(e.target.dataset.id) < Number.parseInt(selectedRange[0]))
 				cover = [...ref.current.children].slice(indexOffset, indexOffset + Number.parseInt(e.target.dataset.id))
-					.concat([...ref.current.children].slice(indexOffset + Number.parseInt(selectedCell[0]) + 1, [...ref.current.children].length))
+					.concat([...ref.current.children].slice(indexOffset + Number.parseInt(selectedRange[0]) + 1, [...ref.current.children].length))
 			else cover = [];
 			cover.forEach(cell => cell.className = flag ? styles['cell-included'] : styles['cell']);
 		}
-	}, [selectedCell, index])
+	}, [selectedRange, index])
 
 	const timePoint = useMemo(() => {
 		const arr = []
@@ -135,26 +148,56 @@ export default function TimePicker({size, zIndex, setTimeRange, title}) {
 		}
 		return arr;
 	}, [handleCellClick, handleHoverHighLight])
-	
+
+	useEffect(() => {
+		let clear;
+		document.addEventListener('click', clear = e => {
+			if (!ref.current.contains(e.target) && e.target !== positionRef.current)
+				setVisible(false);
+		})
+		return () => {
+			document.removeEventListener(clear);
+		};
+	}, [ref, positionRef]);
 	return (
-		<div className={styles['time-picker-wrapper']} style={{zIndex, fontSize, width, height}}>
-			<div className={styles.header}>{title}</div>
-			<div className={styles.container} ref = {ref}>
-				{index.concat(timePoint)}
-			</div>
-		</div>
+		<>
+			{container}
+			{positionRef.current && (
+				<div 
+					className={styles['time-picker-wrapper']} 
+					style={{
+						zIndex, 
+						fontSize, 
+						width, 
+						height, 
+						left: `${(positionRef.current.getBoundingClientRect().left + positionRef.current.getBoundingClientRect().right) / 2}px`,
+						display: `${visible ? 'block' : 'none'}`,
+						top: `${positionRef.current.getBoundingClientRect().top + positionRef.current.getBoundingClientRect().height}px`,
+						transform: 'translateX(-50%)'
+					}}
+				>
+					<div className={styles.header}>{title}</div>
+					<div className={styles.container} ref = {ref}>
+						{index.concat(timePoint)}
+					</div>
+				</div>
+			)}
+
+		</>
 
 	)
 }
 
 TimePicker.propTypes = {
 	zIndex: propTypes.number,
-	setTimeRange: propTypes.func.isRequired,
+	setTime: propTypes.func.isRequired,
 	size: propTypes.oneOf(['small', 'medium', 'big']),
 	title: propTypes.string,
+	attachElement: propTypes.element.isRequired,
 }
 TimePicker.defaultProps = {
 	zIndex: 1,
 	size: 'medium',
-	title: 'react time picker'
+	setTime: () => {},
+	title: 'react time picker',
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { render} from 'react-dom';
 import TimePicker from 'components/time-picker';
-const App = () => (
-    <TimePicker />
-);
+function App() {
+    const [time, setTime] = useState('');
+    return (
+        <div>
+            <TimePicker setTime = {setTime} attachElement={<input style={{position:'absolute',left:'500px',top:'100px',width:'400px', textAlign: 'center'}} value={time} placeholder='input' />}/>
+        </div>
+    )
+};
 render(<App />, document.getElementById("root"));
